c95defa1af59e3929f7c67d75f8326b773846bbb
Yannick <yannick.lebec.dev@gmail.com>
2026-02-13 16:26:54 +0100

    ajout de docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,11 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "200kb" }));
+app.use(express.json());
 
 // health
 app.get("/api/health", (_, res) => res.json({ ok: true }));
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Backend listening on ${PORT}`));
